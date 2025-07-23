e762d4e9bbfa58d716a6d81e4647387aa08ab34e
Karan Kamble <karanbk9375@gmail.com>
2025-07-23 20:18:50 +0530

    Find the median of two sorted arrays of different sizes

--- Project3/src/test/FindMedian.java
+++ Project3/src/test/FindMedian.java
@@ -1,14 +1,14 @@
 package test;
 
 import java.util.Arrays;
-
+    // Time complexity : O(m+n)
 public class FindMedian {
 
     private static float findMedian(int[] arr1, int[] arr2) {
         int[] arr3 = new int[arr1.length + arr2.length];
         int i = 0, j = 0, k = 0;
 
-        while (i < arr1.length && j < arr2.length) {
+        while (i < arr1.length && j < arr2.length && k <= arr3.length/2) {
 
             if (arr1[i] < arr2[j]) {
                 arr3[k] = arr1[i];
@@ -21,13 +21,13 @@ public class FindMedian {
             }
         }
 
-        while (i < arr1.length) {
+        while (i < arr1.length && k <= arr3.length/2 ) {
             arr3[k] = arr1[i];
             i++;
             k++;
         }
 
-        while (j < arr2.length) {
+        while (j < arr2.length && k <= arr3.length/2 ) {
             arr3[k] = arr2[j];
             j++;
             k++;
