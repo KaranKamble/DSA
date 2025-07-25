f6edefa415e65eb608d8b01bfd3178b5544ce696
Karan Kamble <karanbk9375@gmail.com>
2025-07-25 08:44:23 +0530

    BestTimetoBuyandSellStockI V2

--- Project3/src/test/BestTimetoBuyandSellStockI.java
+++ Project3/src/test/BestTimetoBuyandSellStockI.java
@@ -21,9 +21,20 @@ public class BestTimetoBuyandSellStockI {
         System.out.println(maxProfit);
     }
 
+    private static int maxProfitV2(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
-        maxProfit(prices);
+        maxProfitV2(prices);
     }
 
 
