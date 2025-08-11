2e8bd7c4d66b26cbdf11848fb313a8a9aafb0cf1
Karan Kamble <karanbk9375@gmail.com>
2025-08-11 08:33:50 +0530

    SubarrayWithMaximumSumWithTarget with sliding window

---
+++ Project3/src/test/SubarrayWithMaximumSumWithTarget.java
@@ -0,0 +1,30 @@
+package test;
+
+public class SubarrayWithMaximumSumWithTarget {
+
+    private static int subarrayWithMaximumSum(int[] arr, int k) {
+
+        int wSum =0;
+        int cSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < k; i++) {
+            wSum = wSum + arr[i];
+        }
+
+        for (int i = k; i < arr.length; i++) {
+            wSum = wSum - arr[i-k] + arr[i];
+            cSum= Math.max(wSum, cSum);
+        }
+
+        return cSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 9, 31, -4, 21, 7};
+        int k=3;
+
+        System.out.println(subarrayWithMaximumSum(arr,k));
+    }
+
+
+}
